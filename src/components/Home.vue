1b63093487bd6ec6a0d5274a4d91bf73b4904610
groyce <groyce@unomaha.edu>
2020-08-31 21:27:20 -0500

    movie-icon

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -28,7 +28,7 @@
                 <v-flex xs20>
                   <v-card>
                     <v-img
-                      :src="require('@/assets/images/customer.jpg')" height="200px">
+                      :src="require('@/assets/images/movie-icon.jpg')" height="200px">
                     </v-img>
 
                     <v-card-actions>
